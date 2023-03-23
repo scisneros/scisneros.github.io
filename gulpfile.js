eb48960b1a737e05e5ce303a641187b1d23b74df
scisneros <sebastian.cisneros384@gmail.com>
2023-03-23 13:49:47 -0300

    Add gulp watchers

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,22 @@
 var gulp = require('gulp');
 var plumber = require('gulp-plumber');
+var browserSync = require('browser-sync').create()
 const uglify = require('gulp-uglify');
 const sass = require('gulp-sass');
 const wait = require('gulp-wait');
 const babel = require('gulp-babel');;
 const rename = require('gulp-rename');
+
+// Static Server + watching scss/html files
+gulp.task('serve', function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch('./scss/*.scss', gulp.series('styles'));
+    gulp.watch("./*.html").on('change', browserSync.reload);
+});
 
 gulp.task('scripts', function() {
     return gulp.src('./js/scripts.js')
@@ -30,10 +42,13 @@ gulp.task('styles', function () {
     return gulp.src('./scss/*.scss')
         .pipe(wait(250))
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
     gulp.watch('./js/scripts.js', gulp.series('scripts'));
     gulp.watch('./scss/*.scss', gulp.series('styles'));
 });
+
+gulp.task('default', gulp.series('serve'));
